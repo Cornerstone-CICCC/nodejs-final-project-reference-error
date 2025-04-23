630f4d25510ba0e92e9c6b32cfc49f16d566e353
kazuki-0418 <kazuki.castle0418@gmail.com>
2025-04-23 00:08:42 -0700

    fixed bugs

--- frontend/src/app/(with-websocket)/layout-client.tsx
+++ frontend/src/app/(with-websocket)/layout-client.tsx
@@ -1,55 +1,19 @@
-"use client";
 import "../globals.css";
-import { ProtectedRoute } from "@/app/(no-websocket)/auth/hooks/ProtectedRoute";
-import { useAuthStore } from "@/app/(no-websocket)/auth/store/useAuthStore";
-import { WebSocketProvider } from "@/contexts/WebSocketContext";
-import { QuizProvider } from "@/stores/quizStore";
-import { useParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import type { Metadata } from "next";
 
-function AuthProvider({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  const { checkAuth } = useAuthStore();
+export const metadata: Metadata = {
+  title: "Quizoo",
+  description: "Real-time quiz app with a cute animals theme",
+};
 
-  useEffect(() => {
-    checkAuth();
-  }, [checkAuth]);
-
-  return <>{children}</>;
-}
-
-export default function ClientLayout({
+export function ClientLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const params = useParams();
-  const sessionId = typeof params?.sessionId === "string" ? params.sessionId : "";
-
-  const [hasHostString, setHasHostString] = useState(false);
-
-  const checkPathForHostString = () => {
-    const pathName = window.location.pathname;
-
-    const includesHost = pathName.includes("host");
-    return includesHost;
-  };
-
-  // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
-  useEffect(() => {
-    setHasHostString(checkPathForHostString());
-  }, []);
-
   return (
-    <AuthProvider>
-      <WebSocketProvider>
-        <QuizProvider sessionId={sessionId} isHost={hasHostString}>
-          <ProtectedRoute>{children}</ProtectedRoute>
-        </QuizProvider>
-      </WebSocketProvider>
-    </AuthProvider>
+    <html lang="en">
+      <body suppressHydrationWarning={true}>{children}</body>
+    </html>
   );
 }
--- frontend/src/app/(with-websocket)/layout.tsx
+++ frontend/src/app/(with-websocket)/layout.tsx
@@ -1,11 +1,12 @@
-import ClientLayout from "@/app/(no-websocket)/layout-client";
-import type { Metadata } from "next";
+"use client";
+import { WebSocketProvider } from "@/contexts/WebSocketContext";
+import { QuizProvider } from "@/stores/quizStore";
 import { Geist, Geist_Mono } from "next/font/google";
-
-export const metadata: Metadata = {
-  title: "Quizoo",
-  description: "Real-time quiz app with a cute animals theme",
-};
+import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import { ProtectedRoute } from "../(no-websocket)/auth/hooks/ProtectedRoute";
+import { useAuthStore } from "../(no-websocket)/auth/store/useAuthStore";
+import { ClientLayout } from "./layout-client";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -17,14 +18,52 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+function AuthProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { checkAuth } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  return <>{children}</>;
+}
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const params = useParams();
+  const sessionId = typeof params?.sessionId === "string" ? params.sessionId : "";
+
+  const [hasHostString, setHasHostString] = useState(false);
+
+  const checkPathForHostString = () => {
+    const pathName = window.location.pathname;
+
+    const includesHost = pathName.includes("host");
+    return includesHost;
+  };
+
+  // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+  useEffect(() => {
+    setHasHostString(checkPathForHostString());
+  }, []);
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         suppressHydrationWarning={true}
       >
-        <ClientLayout>{children}</ClientLayout>
+        <ClientLayout>
+          <AuthProvider>
+            <WebSocketProvider>
+              <QuizProvider sessionId={sessionId} isHost={hasHostString}>
+                <ProtectedRoute>{children}</ProtectedRoute>
+              </QuizProvider>
+            </WebSocketProvider>
+          </AuthProvider>
+        </ClientLayout>
       </body>
     </html>
   );
